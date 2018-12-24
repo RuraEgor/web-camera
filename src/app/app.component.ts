d797705b25843ee07383a04ec5e9469789ad19b5
unknown <erura@mentalstack.com>
2018-12-24 11:27:27 +0300

    add pers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import construct = Reflect.construct;
+import {Options} from 'ng5-slider';
 // import * as $ from 'jquery';
 // import 'jquery-ui';
 
@@ -9,55 +10,97 @@ import construct = Reflect.construct;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+  
   img1: any;
-  input: any;
-
+  
   fon: any;
   pers_2: any;
   pers_3: any;
-
-  construct() {}
-
+  
+  makeTransBack: boolean = false;
+  persCom_1: boolean = true;
+  persCom_2: boolean = true;
+  
+  value: number = 190;
+  options: Options = {
+    floor: 0,
+    ceil: 256
+  };
+  
+  construct() {
+  }
+  
   ngOnInit() {
+    
     this.img1 = document.getElementById('img1');
-    this.input = <HTMLElement>document.querySelector('.count-color');
-
+    
     this.fon = <HTMLElement>document.querySelector('.fon');
     this.pers_2 = <HTMLElement>document.querySelector('.pers-2');
     this.pers_3 = <HTMLElement>document.querySelector('.pers-3');
-
+    
     this.videoTransp();
   }
-
+  
   videoTransp() {
     let canv1 = <HTMLCanvasElement>document.getElementById('canv1');
-    let countColor = this.input.value;
-
+    let canv2 = <HTMLCanvasElement>document.getElementById('canv2');
+    const countColor = this.value;
+    
     canv1.width = 700;
     canv1.height = 500;
-
+    
+    canv2.width = 700;
+    canv2.height = 500;
+    
     let ctvCanv = canv1.getContext('2d');
-    // let video = <HTMLVideoElement>document.querySelector('video');
-
-    // ctvCanv.drawImage(video, 0, 0, ctvCanv.canvas.width, ctvCanv.canvas.height);
-
-    ctvCanv.drawImage(this.fon, 0, -190);
-
-    let imgData = ctvCanv.getImageData(0, 0, ctvCanv.canvas.width, ctvCanv.canvas.height);
+    let ctvCanv_2 = canv2.getContext('2d');
+    let video = <HTMLVideoElement>document.querySelector('video');
+    
+    ctvCanv_2.drawImage(video, 0, 0, ctvCanv.canvas.width, ctvCanv.canvas.height);
+    
+    let imgData = ctvCanv_2.getImageData(0, 0, ctvCanv.canvas.width, ctvCanv.canvas.height);
     let pixels = imgData.data;
-
-    // for (let i = 0; i < pixels.length; i += 4) {
-    //   if (pixels[i] > countColor && pixels[i + 1] > countColor && pixels[i + 2] > countColor) pixels[i + 3] = 0;
-    // }
-
-    ctvCanv.putImageData(imgData, 0, 0);
-
-
-    ctvCanv.drawImage(this.pers_2, -10, 40);
-    ctvCanv.drawImage(this.pers_3, 430, 40);
-
-
+    
+    if (this.makeTransBack) {
+      for (let i = 0; i < pixels.length; i += 4) {
+        if (pixels[i] > countColor && pixels[i + 1] > countColor && pixels[i + 2] > countColor) pixels[i + 3] = 0;
+      }
+    }
+    
+    ctvCanv_2.putImageData(imgData, 0, 0);
+    
+    ctvCanv.drawImage(this.fon, 0, -190);
+    
+    ctvCanv.drawImage(canv2, 0, 0);
+    
+    if (this.persCom_1) {
+      ctvCanv.drawImage(this.pers_2, -10, 40);
+    }
+  
+    if (this.persCom_2) {
+      ctvCanv.drawImage(this.pers_3, 430, 40);
+    }
+    
     window.requestAnimationFrame(this.videoTransp.bind(this));
+  }
+  
+  
+  makeTrans() {
+    this.makeTransBack = !this.makeTransBack;
+  }
+  
+  perst1() {
+    this.persCom_1 = !this.persCom_1;
+  }
+  
+  perst2() {
+    this.persCom_2 = !this.persCom_2;
+  }
+  
+  takeFoto() {
+    let canv1 = <HTMLCanvasElement>document.getElementById('canv1');
+    let ctvCanv = canv1.getContext('2d');
+    let fotoReady = <HTMLImageElement>document.querySelector('#foto-ready');
+    fotoReady.src = canv1.toDataURL("image/png");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WebcamModule } from 'ngx-webcam';
+import { Ng5SliderModule } from 'ng5-slider';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { WebcamModule } from 'ngx-webcam';
   ],
   imports: [
     BrowserModule,
-    WebcamModule
+    WebcamModule,
+    Ng5SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
